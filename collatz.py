06e9c46752ad744ec1b982b3622b53e348f1a73b
frost289 <bizimanaliforce.gmail.com>
2026-03-26 04:18:32 +0200

    divide even numbers by 2. multiples by 3 and add 1 if the number is odd.

---
+++ collatz.py
@@ -0,0 +1,19 @@
+#if the number is even, divide by 2
+# if it odd nultiple by 3 and add 1.. while n is not equal to one
+
+def colla(n):
+    
+    while n != 1:
+        yield n
+        n = n // 2 if n %2 == 0 else n * 3 + 1
+
+        
+    yield 1
+
+
+
+
+
+
+n = pow(10,1000)
+print(tuple(colla(n)))
