0ca36031f6d0c92def3e140b1bdc14141f46101c
frost289 <bizimanaliforce.gmail.com>
2026-03-27 17:29:21 +0200

    fix the maximum charecters that can randomly be generated by adding the len() instead of using length

--- passwordGen.py
+++ passwordGen.py
@@ -1,10 +1,14 @@
 import random
-
+import re
 def generate_password(length):
     characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-
+    n = []
     for i in range(0,length + 1):
-        print(characters[random.randint(0,length + 1)])
+        n.append(characters[random.randint(0,len(characters)-1)])
+    str(n)
+    print(n)
+
+
 
 
 generate_password(5)
